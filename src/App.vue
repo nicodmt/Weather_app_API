dbcc92f081af498f12e1fe7a38e6d747141e8c2e
nicodmt <dumont.nicolas.94@gmail.com>
2020-08-24 11:32:48 +0200

    Added some css to search box just for fun

--- src/App.vue
+++ src/App.vue
@@ -62,6 +62,15 @@ main{
   outline: none;
   background: none;
 
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:hover{
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
 }
 </style>
