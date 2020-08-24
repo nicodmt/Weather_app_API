12c06e102d3cac583ad7a11e265c5a8fd50e6f64
nicodmt <dumont.nicolas.94@gmail.com>
2020-08-24 11:17:52 +0200

    Added the API key before i lose it and a search bar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..." />
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      api_key: '351cfc00abfbb2b710dc0c8a2af6364d'
+    }
+  }
 }
 </script>
 
 <style>
+
 </style>
