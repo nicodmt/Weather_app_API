ccefebf498197ccf575098de578d118f1f794e36
nicodmt <dumont.nicolas.94@gmail.com>
2020-08-24 11:42:57 +0200

    Added CSS properties to location box and box within

--- src/App.vue
+++ src/App.vue
@@ -85,4 +85,20 @@ main{
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
 }
+
+.location-box .location{
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date{
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
 </style>
